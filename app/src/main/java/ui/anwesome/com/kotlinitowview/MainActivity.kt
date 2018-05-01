2b4b5d9380d6326433022a8d80d9ee993a901a6d
Anwesh43 <anweshthecool0@gmail.com>
2018-05-01 11:01:46 +0530

    adding fullscreen support

--- app/src/main/java/ui/anwesome/com/kotlinitowview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinitowview/MainActivity.kt
@@ -2,6 +2,7 @@ package ui.anwesome.com.kotlinitowview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import ui.anwesome.com.itowview.IToWView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         IToWView.create(this)
+        fullScreen()
     }
+}
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }

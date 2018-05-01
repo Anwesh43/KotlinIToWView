133aeb33d5497a5c84322ab9d770f977b6831f74
Anwesh43 <anweshthecool0@gmail.com>
2018-05-01 10:59:04 +0530

    adding Activity in View class

--- app/src/main/java/ui/anwesome/com/kotlinitowview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinitowview/MainActivity.kt
@@ -2,10 +2,12 @@ package ui.anwesome.com.kotlinitowview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.itowview.IToWView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        IToWView.create(this)
     }
 }
